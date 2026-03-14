ec6317fc2e3a772bf97533eeb42114888f0c1c51
n0x90 <t.morgan42@icloud.com>
2026-03-14 14:47:20 +0000

    Remove theme detection

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,40 +1,3 @@
-local function get_gnome_appearance()
-  local handle = io.popen(
-    "sh -c 'gsettings get org.gnome.desktop.interface color-scheme 2>/dev/null || gsettings get org.gnome.desktop.interface gtk-theme 2>/dev/null'"
-  )
-  if not handle then
-    return "latte"
-  end
-
-  local result = handle:read("*a") or ""
-  handle:close()
-
-  return result:match("[Dd]ark") and "mocha" or "frappe"
-end
-
-local function get_macos_appearance()
-  local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
-  if not handle then
-    return "latte"
-  end
-
-  local result = handle:read("*a") or ""
-  handle:close()
-
-  return result:match("[Dd]ark") and "mocha" or "latte"
-end
-
-local function get_system_background()
-  local uname = vim.loop.os_uname().sysname or ""
-  if uname == "Darwin" then
-    return get_macos_appearance()
-  end
-  if uname == "Linux" then
-    return get_gnome_appearance()
-  end
-  return "mocha"
-end
-
 return {
   "catppuccin/nvim",
   name = "catppuccin",
@@ -43,7 +6,7 @@ return {
 
   opts = function()
     return {
-      flavour = get_system_background(),
+      flavour = "mocha",
       background = {
         light = "latte",
         dark = "mocha",

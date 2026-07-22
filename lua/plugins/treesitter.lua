e112e852d0e23537f7c64fe044589ca940f70f32
n0x90 <t.morgan42@icloud.com>
2026-07-21 19:17:42 -0500

    Fix treesitter startup parser installs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   lazy = false,
   build = ":TSUpdate",
   opts = {
@@ -21,6 +22,7 @@ return {
       "vimdoc",
       "yaml",
     },
+    auto_install = false,
     highlight = {
       enable = true,
     },
@@ -30,40 +32,6 @@ return {
     },
   },
   config = function(_, opts)
-    local ts = require("nvim-treesitter")
-    ts.setup(opts)
-    ts.install(opts.ensure_installed)
-
-    local group = vim.api.nvim_create_augroup("treesitter_filetypes", { clear = true })
-
-    vim.api.nvim_create_autocmd("FileType", {
-      group = group,
-      pattern = "*",
-      callback = function(event)
-        local filetype = vim.bo[event.buf].filetype
-        local lang = vim.treesitter.language.get_lang(filetype)
-
-        if not lang then
-          return
-        end
-
-        if opts.highlight.enable and vim.treesitter.query.get(lang, "highlights") then
-          pcall(vim.treesitter.start, event.buf, lang)
-        end
-
-        local indent_disabled = opts.indent.disable or {}
-        if not opts.indent.enable then
-          return
-        end
-
-        if vim.tbl_contains(indent_disabled, filetype) or vim.tbl_contains(indent_disabled, lang) then
-          return
-        end
-
-        if vim.treesitter.query.get(lang, "indents") then
-          vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
-      end,
-    })
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
